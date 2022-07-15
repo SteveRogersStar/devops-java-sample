d60e83ba900cd709ce31bac7a398bf1bbf50ad32
steverogers <heyu-998@163.com>
2022-07-15 17:52:49 +0800

    Update HelloWorldController.java

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -14,6 +14,6 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello() {
-        return "hello world,steverogers!";
+        return "hello world,kubesphere!";
     }
 }
